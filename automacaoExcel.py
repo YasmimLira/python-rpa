6201cb73a484d6464e8946927627345aec6c952b
unknown <micayaya70@gmail.com>
2024-03-01 16:35:56 -0300

    Adicionando agendamento de horário

--- automacaoExcel.py
+++ automacaoExcel.py
@@ -2,13 +2,16 @@ from selenium import webdriver as webdriver
 from selenium.webdriver.common.by import By
 import pyautogui as action
 import pyautogui as loadTime
+import schedule
+import time
+import datetime
 
 navegadorFake = webdriver.Chrome()
 
 navegadorFake.get('https://totvs-torres.monday.com/')
 
 loginInput = navegadorFake.find_element(By.ID, 'user_email').send_keys('teste@gmail.com')
-passwordInput = navegadorFake.find_element(By.ID, 'user_password').send_keys('11111')
+passwordInput = navegadorFake.find_element(By.ID, 'user_password').send_keys('00000000')
 loadTime.sleep(2)
 
 loginButton = navegadorFake.find_element(By.CLASS_NAME, 'next-button-component').click()
@@ -28,7 +31,6 @@ loadTime.sleep(4)
 texto_alvo = "Mais ações"
 elemento = navegadorFake.find_element(By.XPATH, f'//*[text()="{texto_alvo}"]').click()
 
-#moreActionButton = navegadorFake.find_element(By.CSS_SELECTOR, '').click()
 loadTime.sleep(3)
 
 text_excel = "Exportar quadro para o Excel"
@@ -41,4 +43,20 @@ loadTime.sleep(2)
 
 sendButton = navegadorFake.find_element(By.XPATH, '/html/body/div[55]/div/div/div/div/div/div/div[3]/button').click()
 
+def minha_tarefa():
+    print("Executando minha tarefa em", datetime.datetime.now())
+#Agendando a tarefa para executar em uma data e horário específicos
+#schedule.every().day.at("10:00").do(minha_tarefa)
+
+#Altere "10:00" para o horário desejado
+schedule.every().monday.at("10:00").do(minha_tarefa)
+
+# Para executar toda segunda-feira às 10:00, por exemplo
+# Loop para continuar verificando se há tarefas a serem executadas
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+
 loadTime.sleep(10)
+
+
